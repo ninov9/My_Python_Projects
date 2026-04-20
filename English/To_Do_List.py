7348e7e4bb106794271a8ee31ecfca7493ef2c95
ninov9 <ninovilletet@gmail.com>
2026-04-21 01:11:10 +0200

    Add To_Do_List.py

--- English/To_Do_List.py
+++ English/To_Do_List.py
@@ -7,22 +7,10 @@ class ExitToMainException(Exception):
 def run_program5():
 
     import os
-    import time
-    import sys
+    from time import sleep
     from colorama import Fore
     import json
     
-    
-    # DEFAULT SETTINGS
-
-    Fore.WHITE
-    global list_tasks
-    options = [0, 1, 2, 3, 4, 5]
-    
-    
-    def sleep(seconds):
-        time.sleep(seconds)
-
 
     def write(sentence):
         for char in sentence:
@@ -45,28 +33,19 @@ def run_program5():
             return []
         except json.JSONDecodeError:
             write_red("Error decoding JSON file. The file might be corrupted.")
-            sleep(3)
+            sleep(1)
+            input(Fore.CYAN + "Press Enter to continue..." + Fore.RESET)
             return []
     
     
     list_tasks = load_tasks()
     
-    task_name = ""
-    
-    
     # EVERY ACTIONS
     
     def save_tasks():
-        global list_tasks
         with open("list_tasks.json", "w") as file:
             json.dump(list_tasks, file)
-    
- 
-    def erase(n):
-        for _ in range(n):
-            sys.stdout.write("\033[F")
-            sys.stdout.write("\033[K")
-    
+
     
     def clear():
         os.system('cls' if os.name == 'nt' else 'clear')
@@ -74,33 +53,33 @@ def run_program5():
     
     def write_green(sentence):
         write(Fore.LIGHTGREEN_EX + sentence + Fore.RESET) 
+
     
-    
+    def display_list():
+        if not list_tasks:
+            write_green("Your TO-DO List is empty.")
+        else:
+            write_green("You have " + Fore.LIGHTWHITE_EX + str(len(list_tasks)) + Fore.GREEN + " tasks to do:" + Fore.RESET)
+            for i, task in enumerate(list_tasks, 1):
+                write_green("\n\n{}. {}".format(i, task))
+
+
     def empty():
-        clear()
-        sleep(0.5)
         list_tasks.clear()
-        check_list()
-        choice_option()
+        write_green("\nThe list has been emptied successfully !\n")
+        input(Fore.CYAN + "\n\nPress Enter to continue..." + Fore.RESET)
+        save_tasks()
     
     
     def add_task():
-        clear()
-        sleep(0.5)
-        task_name = input(Fore.CYAN + "Enter the name of the task : " + Fore.RESET)
+        task_name = input(Fore.CYAN + "\n\nEnter the name of the task you want to add: " + Fore.RESET)
         list_tasks.append(task_name)
-        clear()
-        sleep(0.5)
-        check_list()
-        choice_option()
+        write_green(f"\nTask '{task_name}' added successfully !\n")
+        input(Fore.CYAN + "\n\nPress Enter to continue..." + Fore.RESET)
+        save_tasks()
     
     
-    def remove_task():
-        clear()
-        sleep(0.5)
-        clear()
-        check_list()
-        sleep(0.5)
+    def remove_task():  
         while True:
             try:
                 index_task = int(input(Fore.CYAN + "\n\nNumber of the task you want to remove: " + Fore.RESET))
@@ -108,134 +87,85 @@ def run_program5():
                     break
                 else:
                     write_red("\nChoose an existing task!")
-                    sleep(2)
-                    erase(4)
             except ValueError:
                 write_red("\nPlease enter a valid digit!")
-                sleep(2)
-                erase(4)
-        clear()
-        sleep(0.5)
+            sleep(1.5)
+            clear()
+            display_list()
+        write_green(f"\nTask '{list_tasks[index_task - 1]}' removed successfully !\n")
         list_tasks.pop(index_task - 1)
-        clear()
-        sleep(0.5)
-        check_list()
-        choice_option()
+        input(Fore.CYAN + "\n\nPress Enter to continue..." + Fore.RESET)
+        save_tasks()
     
     
     def edit_task():
-        clear()
-        sleep(0.5)
-        clear()
-        check_list()
-        sleep(0.5)
-        try:
-            index_task = int(input(Fore.CYAN + "\n\nNumber of the task do you want to edit : " + Fore.RESET))
-        except ValueError:
-            write_red("\nPlease enter a digit !")
-            sleep(2)
-            erase(4)
-            edit_task()
-        if 1 <= index_task <= len(list_tasks):
-            pass
-        else:
-            write_red("\nChoose an existing task !")
-            sleep(2)
-            erase(4)
-            edit_task()
-        clear()
-        sleep(0.5)
-        list_tasks[index_task - 1] = input(Fore.CYAN + "Enter the new name of the task : " + Fore.RESET)
-        clear()
-        sleep(0.5)
-        check_list()
-        choice_option()
+        while True:
+            try:
+                index_task = int(input(Fore.CYAN + "\n\nNumber of the task you want to edit: " + Fore.RESET))
+                if 1 <= index_task <= len(list_tasks):
+                    break
+                else:
+                    write_red("\nChoose an existing task!")
+            except ValueError:
+                write_red("\nPlease enter a digit!")
+            
+            sleep(1.5)
+            clear()
+            display_list()
+
+        list_tasks[index_task - 1] = input(Fore.CYAN + f"\nEnter the new name of the task N°{index_task}: " + Fore.RESET)
+        write_green(f"\nTask N°{index_task} edited successfully!\n")
+        input(Fore.CYAN + "\n\nPress Enter to continue..." + Fore.RESET)
+        save_tasks()
     
     
     def help():
         clear()
-        sleep(0.5)
         write_green("Here are all the available options : ")
-        sleep(2)
-        print("")
-        write_green("\n0 = HELP\n1 = ADD TASK\n2 = REMOVE TASK\n3 = EDIT TASK\n4 = EMPTY LIST\n5 = EXIT PROGRAM")
-        sleep(6)
-        clear()
-        check_list()
-        choice_option()
+        write_green("\n\n0 = HELP\n1 = ADD TASK\n2 = REMOVE TASK\n3 = EDIT TASK\n4 = EMPTY LIST\n5 = EXIT PROGRAM")
+        input(Fore.CYAN + "\n\n\nPress Enter to continue..." + Fore.RESET)
     
     
     def exit_program():
         clear()
-        sleep(0.1)
-        write_green("\nExiting...")
+        write_green("Exiting...")
         sleep(1.5)
         raise ExitToMainException()
     
     
-    # THE USER CHOOSE AN OPTION
+    # THE USER CHOOSES AN OPTION
     
-    def choice_option():
-        global list_tasks
-        while True:
-            try:
-                choice = int(input(Fore.CYAN + "\n\nChoose an option (0 for help) : " + Fore.RESET))
-            except ValueError:
-                write_red("\nPlease enter a valid digit !")
-                sleep(2)
-                erase(4)
-                continue
-            if choice in options:
-                if choice == options[0]:
-                    help()
-                elif choice == options[1]:
-                    add_task()
-                elif choice == options[2]:
-                    if len(list_tasks) == 0:
-                        write_green("\nThe list is empty.")
-                        sleep(2)
-                        clear()
-                        check_list()
-                        choice_option()
-                    else:
-                        remove_task()
-                elif choice == options[3]:
-                    if len(list_tasks) == 0:
-                        write_green("\nThe list is empty.")
-                        sleep(2)
-                        clear()
-                        check_list()
-                        choice_option()
-                    else:
-                        edit_task()
-                elif choice == options[4]:
-                    empty()
-                elif choice == options[5]:
-                    exit_program()
-                    break
-            else:
-                write_red("\nChoose a valid digit !")
-                sleep(2)
-                erase(4)
-    
-    
-    # CHECK LIST
-    
-    def check_list():
-        global list_tasks
-        save_tasks()
-
-        if not list_tasks:
-            write_green("Your TO-DO List is empty.")
+    while True:
+        clear()
+        display_list()
+        try:
+            choice = int(input(Fore.CYAN + "\n\n\nChoose an option (0 for help) : " + Fore.RESET))
+        except ValueError:
+            write_red("\nPlease enter a valid digit !")
+            sleep(1.5)
+            continue
+        if 0 <= choice <= 5:
+            if choice == 0:
+                help()
+            elif choice == 1:
+                add_task()
+            elif choice == 2:
+                if not list_tasks:
+                    write_green("\nThe list is empty.")
+                    sleep(2)
+                else:
+                    remove_task()
+            elif choice == 3:
+                if not list_tasks:
+                    write_green("\nThe list is empty.")
+                    sleep(2)
+                else:
+                    edit_task()
+            elif choice == 4:
+                empty()
+            elif choice == 5:
+                exit_program()
+                break
         else:
-            write_green("You have " + Fore.LIGHTWHITE_EX + str(len(list_tasks)) + Fore.GREEN + " tasks to do:" + Fore.RESET)
-            for i, task in enumerate(list_tasks, 1):
-                write_green("\n\n{}. {}".format(i, task))
-    
-    
-    # START
-    
-    clear()
-    check_list()
-    choice_option()
-    
+            write_red("\nChoose a valid digit !")
+            sleep(1.5)
