74f21be65867c747677ff7db4bfe87f1e00dff76
ninov9 <ninovilletet@gmail.com>
2026-04-24 00:09:28 +0200

    Update main.py

--- English/main.py
+++ English/main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from time import sleep
+
 # Import the programs
 import Guess_the_number
 import Hangman
@@ -19,11 +20,8 @@ def clear():
 
 def main():
 
-    clear()
-    sleep(1)
     while True:
         clear()
-        sleep(1)
         # List all the programs
         print("1. Guess The Number")
         print("2. Hangman Game")
@@ -36,38 +34,37 @@ def main():
         try:
             # The user chooses which program to run
             choice = int(input("\nEnter your choice : "))
-            if choice == 1:
-                Guess_the_number.run_program1()
-            elif choice == 2:
-                Hangman.run_program2()
-            elif choice == 3:
-                Random_Password.run_program3()
-            elif choice == 4:
-                Rock_Paper_Scissors.run_program4()
-            elif choice == 5:
-                try:
-                    To_Do_List.run_program5()
-                except ExitToMainException:
-                    continue
-            elif choice == 6:
-                Encoder_Decoder.run_program6()
-            elif choice == 0:
-                print("\nExiting...")
-                sleep(2)
-                sys.exit()
-
-            else:
-                print("Please choose an existing program.")
-                sleep(2)
-                clear()
         # Handle value error
         except ValueError:
             print("Please use an integer.")
             sleep(2)
-            clear()
+            continue
+
+        if choice == 1:
+            Guess_the_number.run_program1()
+        elif choice == 2:
+            Hangman.run_program2()
+        elif choice == 3:
+            Random_Password.run_program3()
+        elif choice == 4:
+            Rock_Paper_Scissors.run_program4()
+        elif choice == 5:
+            try:
+                To_Do_List.run_program5()
+            except ExitToMainException:
+                continue
+        elif choice == 6:
+            Encoder_Decoder.run_program6()
+        elif choice == 0:
+            print("\nExiting...")
+            sleep(2)
+            sys.exit()
+
+        else:
+            print("Please choose an existing program.")
+            sleep(2)
 
 
 # Run the program
 if __name__ == '__main__':
-    clear()
     main()
